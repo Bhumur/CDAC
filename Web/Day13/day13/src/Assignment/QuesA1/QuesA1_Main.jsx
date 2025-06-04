0f063881afdc7af11f561490f22b3097929d6c66
bhumur-rough <agrawalbhumur@gmail.com>
2025-06-04 20:10:21 +0530

    day13 question a1 server client

--- Web/Day13/day13/src/Assignment/QuesA1/QuesA1_Main.jsx
+++ Web/Day13/day13/src/Assignment/QuesA1/QuesA1_Main.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import axios from 'axios';
 import FruitContext from "./FruitContext"
 import Fruit from "./Fruit"
@@ -7,24 +7,40 @@ import PrintList from "./PrintList"
 const QuesA1_Main = () => {
     const [fruit, setFruit] = useState([])
     const saveFruit = async(data) => {
-        setFruit([...fruit, data])
-        handelReq({data})
+        //setFruit([...fruit, data])
+        try{
+            console.log("save")
+            await toTheServer({data})
+        }catch(error){
+            console.error('Error sending data:', error);
+        }
     }
 
-    const handelReq = async(d) =>{
+
+    const toTheServer = async(d) =>{
         try {
-                const response = await axios.post('http://localhost:5000/fruit', d);
-                console.log(response.data);
+                await axios.post('http://localhost:5000/fruit', d);
+                console.log("toserver");
             } catch (error) {
                 console.error('Error sending data:', error);
             }
-            
+    }
+
+    const fromServer = async() =>{
+        try{
+            const response = await axios.get('http://localhost:5000/f');
+            console.log(response.data)
+            setFruit(response.data)
+        }catch(error){
+            console.error('Error fetching data:', error);
+        }
     }
 
     return(
             <>
                 <FruitContext.Provider value={{fruit : fruit, saveFruit : saveFruit}}>
                     <Fruit/>
+                    <button onClick={(e)=>{fromServer()}}>print</button>
                     <PrintList/>
                 </FruitContext.Provider>
             </>
